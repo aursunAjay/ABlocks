281180f63236d2b576d8fa4d3ed4b01ca0e760e4
aursunAjay <ajay15014@gmail.com>
2023-08-19 12:31:51 +0530

    Information is updated.

--- boot.py
+++ boot.py
@@ -1,47 +1,62 @@
-import network
+from machine import UART
+import bluetooth
+import ble_uart_peripheral
+import settings 
+import _thread
 import machine
-import settings
-print("boot file")
+import time
 
-import uasyncio as asyncio
-import usocket as socket
-import machine
-async def handle_client(client):
-    while True:
-        data = await client.readline()
-        if not data:
-            machine.Pin(2, machine.Pin.OUT).value(1)  # Turn on LED 1
-        else    
-            print("Received:", data)
-            await client.write(data)
+LED_PIN_1 = 2  # GPIO pin for LED 1
+LED_PIN_2 = 4  # GPIO pin for LED 2
 
-async def server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((settings.__IP__, 21))
-    server_socket.listen(1)
-    print("Server listening on port 8080")
+# Initialize the BLE stack
+ble = bluetooth.BLE()
 
-    while True:
-        client, addr = server_socket.accept()
-        print("Connected by", addr)
-        asyncio.create_task(handle_client(client))
+# Initialize the BLEUART peripheral
+ble_uart = ble_uart_peripheral.BLEUART(ble)
 
+data = ""
 
+# Define a callback function to handle received data
+activate_rec = 0
 
-# verify new firmware exit or not
-if settings.__FIRMWARE_STATUS__ == settings.__NEW_FIRMWARE__:
-    print("New firmware!")
-    settings.__FIRMWARE_TOGGLE__ = not settings.__FIRMWARE_TOGGLE__
-    print(settings.__FIRMWARE_TOGGLE__)
+def on_uart_rx():
+    global data
+    global activate_rec
+    check = ble_uart.read()   
     
-if settings.__FIRMWARE_MODE__ == settings.__F1__:
-    print("Enter in F1!")
-    # Open the files using 'with' to ensure proper closing
-    #with open(settings.__F2__, "w") as into_file, open(settings.__F1__, "r") as get_file:
-    #    into_file.write(get_file.read())
-    settings.run(settings.__F1_NAME__) 
-
+    if check == '~':
+         activate_rec = 1
+   
     
-loop = asyncio.get_event_loop()
-loop.create_task(server())
-loop.run_forever()
+    if activate_rec == 1:
+        data+=check.decode("utf-8")
+        check = ""
+    
+    ble_uart.write(check)
+    
+    if check == '#' and data != "":
+        print(data)
+        settings.file(data,"w")
+        data = ""
+        activate_rec = 0
+        settings.run("main.py")
+        
+# Set the callback function
+ble_uart.irq(on_uart_rx)
+
+while True:
+        try:
+         while True:
+          machine.Pin(LED_PIN_1, machine.Pin.OUT).value(1)  # Turn on LED 1
+          time.sleep(1)  # Delay for 1 second
+          machine.Pin(LED_PIN_1, machine.Pin.OUT).value(0)  # Turn off LED 1
+          time.sleep(1)  # Delay for 1 second          
+        except KeyboardInterrupt:
+          pass
+  
+ 
+ble_uart.close()
+
+
+
--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ __NEW_FIRMWARE__ 	 = 1   # Constants for firmware selection
 
 __F1_NAME__ 	= "f1.py"
 __F2_NAME__ 	= "main.py"
-__TEMP_BUFFER__ = "temp_buffer.txt"
+__TEMP_BUFFER__ = "main.py"
 
 __WIFI_STATUS__      = 0   #standard wifi status flag              1 > signals 0 > no signals   
 
@@ -30,18 +30,18 @@ __FIRMWARE_MODE__    = 0   #Standard firmware toggle flag          1 > f1.py   0
 
 __FIRMWARE_STATUS__  = 0   # standard firmware update status flag  1 > new firmware  0 > no update
 
-
+############################################
 def run(runfile):
     with open(runfile, "r") as rnf:
         exec(rnf.read())
-    
+############################################    
 def encrypt(text, key):
     encrypted = ''
     for char in text:
         encrypted_char = chr(ord(char) ^ key)
         encrypted += encrypted_char
     return encrypted
-
+############################################
 def decrypt(ciphertext, key):
     decrypted = ''
     for char in ciphertext:
@@ -49,32 +49,24 @@ def decrypt(ciphertext, key):
         decrypted += decrypted_char
     return decrypted    
 
-# Write data to a file
+# Write data to a file ####################
 def file(data,flag):
  try:        
-    with open(file_path,flag) as file:
+    with open(__TEMP_BUFFER__,flag) as file:
         file.write(data)
         print("Data written to the file")
  except OSError:
         print("Error reading the file")
-        
+###########################################        
 def read_from_file():
     try:
-        with open(file_path, "r") as file:
+        with open(__TEMP_BUFFER__, "r") as file:
             data = file.read()
             print("Data read from the file:", data)
             return data
     except OSError:
         print("Error reading the file")
                
+############################################
 
-p2313123131231=Pin(2,Pin.OUT)
-__CONNECTION__=network.WLAN(network.AP_IF)
-__CONNECTION__.config(essid="SoniBlocks") 
-__CONNECTION__.config(max_clients=5)
-__CONNECTION__.active(True)
-__SOCKET__ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-__SOCKET__.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-__SOCKET__.bind(__IP__,21))
-__SOCKET__.setblocking(False) 
-__SOCKET__.listen(5)
+
